8aa2675b2f0df7f28c5179031de6a4fb082aaebf
James K <jkrrv@jkrrv.org>
2018-01-25 02:57:02 -0600

    Better definitions to improve resiliency and reduce IDE errors

--- display.js
+++ display.js
@@ -8,6 +8,50 @@ let eventList = [],
     currentSlide = null,
     nextSlide = null;
 
+
+Date.prototype.toDateStringFormatted = function() {
+    /* Today */
+    if (this.isToday()) {
+        if (parseInt(this.format('G')) >= 17) { // 5pm or later
+            return "Tonight";
+        } else {
+            return "Today";
+        }
+    }
+
+    /* Tomorrow */
+    if (Date.today().addDays(1).format('Ymd') === this.format('Ymd')) {
+        return "Tomorrow";
+    }
+
+    /* This week */
+    if (this.getTime() - Date.today().getTime() < 7 * 86400 * 1000) {
+        return "This " + this.format('l');
+    }
+
+    /* Next week */
+    if (this.getTime() - Date.today().getTime() < 14 * 86400 * 1000) {
+        return "Next " + this.format('D, M %e');
+    }
+
+    /* Other Times */
+    return this.format('l, M %e');
+};
+
+
+Date.prototype.toTimeStringFormatted = function() {
+    /** @var String string */
+    let string = this.format('%r');
+
+    if (string[0] === '0')
+        string = string.substr(1);
+
+    string = string.replace(' ', '').toLowerCase();
+
+    return string;
+};
+
+
 function updateEventList() {
     const d = new Date();
     const d1 = d.format('%b+d%2C+%Y');
@@ -24,6 +68,9 @@ function updateEventList() {
             alreadyIncludedSlugs = [];
 
         for (let ri in table.childNodes) {
+            if (!table.childNodes.hasOwnProperty(ri))
+                continue;
+
             switch (table.childNodes[ri].nodeName) {
                 case 'thead':
                     date = table.childNodes[ri].childNodes[1].textContent + " 2018"; // TODO figure out a way to deduce the year.
@@ -31,6 +78,9 @@ function updateEventList() {
 
                 case 'tbody':
                     for (let ei in table.childNodes[ri].childNodes) {
+                        if (!table.childNodes[ri].childNodes.hasOwnProperty(ei))
+                            continue;
+
                         if (table.childNodes[ri].childNodes[ei].nodeName !== 'tr')
                             continue;
 
@@ -115,10 +165,12 @@ function changeSlide() {
         nextSlide.innerHTML = "<div><h2>Welcome to Tenth</h2></div>";
         nextSlide.classList.add('welcome');
         nextSlide.style.backgroundPositionY = "100%";
+        nextSlide.style.backgroundPositionX = "0";
         nextSlide.time = 8000;
         let _slide = nextSlide;
         nextSlide.onShow = function() {
             _slide.style.backgroundPositionY = "0";
+            _slide.style.backgroundPositionX = "100%";
         }
 
     } else {
@@ -162,6 +214,7 @@ function changeSlide() {
             else
                 html += "<div><h2>Events This Sunday</h2><table>";
 
+            /** @var Date event.dtStart */
             while (event.dtStart.format('D') === 'Sun' && Date.today().getWeek() === event.dtStart.getWeek() && count++ < 7) {
                 html += "<tr><td class='right'>" + event.dtStart.toTimeStringFormatted() + "</td><td>" + event.title + "</td><td>" + event.location + "</td></tr>";
 
@@ -185,7 +238,6 @@ function changeSlide() {
         }
     }
 
-
     nextEventToSlideify++;
     if (nextEventToSlideify >= eventList.length) {
         updateEventList();
@@ -202,47 +254,3 @@ function getTense(eventObj) {
     const now = new Date();
     return eventObj.dtStart.compareTo(now) + eventObj.dtEnd.compareTo(now);
 }
-
-Date.prototype.toDateStringFormatted = function() {
-    /* Today */
-    if (this.isToday()) {
-        if (parseInt(this.format('G')) >= 17) { // 5pm or later
-            return "Tonight";
-        } else {
-            return "Today";
-        }
-    }
-
-    console.log(this.getTime() - Date.today().getTime());
-
-    /* Tomorrow */
-    if (Date.today().addDays(1).format('Ymd') === this.format('Ymd')) {
-        return "Tomorrow";
-    }
-
-    /* This week */
-    if (this.getTime() - Date.today().getTime() < 7 * 86400 * 1000) {
-        return "This " + this.format('l');
-    }
-
-    /* Next week */
-    if (this.getTime() - Date.today().getTime() < 14 * 86400 * 1000) {
-        return "Next " + this.format('D, M %e');
-    }
-
-    /* Other Times */
-    return this.format('l, M %e');
-};
-
-
-Date.prototype.toTimeStringFormatted = function() {
-    let string = this.format('%r');
-
-    if (string[0] === '0')
-        string = string.substr(1);
-
-    string = string.replace(' ', '').toLowerCase();
-
-    return string;
-};
-
