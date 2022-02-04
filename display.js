f3f25fcec52fb4a401190c5136d2687259066add
James K <jkrrv@jkrrv.org>
2022-02-03 20:21:53 -0500

    Library updates and sourcing announcements from WordPress

--- display.js
+++ display.js
@@ -57,7 +57,9 @@ function updateEventLists() {
     let d0 = new Date(),
         d1 = new Date().addMonths(3);
 
-    request("https://www.tenth.org/wp-json/tribe/events/v1/events?featured=true&start_date=" + d0.toString("yyyy-MM-dd") + "&end_date=" + d1.toString("yyyy-MM-dd") + "&hide_subsequent_recurrences=1&per_page=200", featuredCallback);
+    request("https://www.tenth.org/wp-json/tribe/events/v1/events?featured=true&start_date=" +
+        d0.toString("yyyy-MM-dd") + "&end_date=" + d1.toString("yyyy-MM-dd") +
+        "&hide_subsequent_recurrences=1&per_page=200", featuredCallback);
     // request('https://www.tenth.org/events/all-json', allCallback);
 
     function featuredCallback(error, response) {
@@ -236,7 +238,7 @@ function changeSlide() {
                 nextSlide.innerHTML = "";
             }
             nextSlide.classList.add('single');
-            nextSlide.time = 4000;
+            nextSlide.time = 6000;
 
             if (event.hasOwnProperty('imageUrl') && event.imageUrl !== null)
                 nextSlide.style.backgroundImage = "url('" + event.imageUrl + "')";
